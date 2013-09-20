336237e19fbbe539b135fea018b6c61fdd94f0e8
xianfeng wang <wang.fl1429@gmail.com>
2013-09-21 01:16:37 +0800

    Update mailer.rb

--- lib/exception_log/mailer.rb
+++ lib/exception_log/mailer.rb
@@ -2,13 +2,13 @@
 module ExceptionLog
 	class Mailer < ::ActionMailer::Base
 
-		def exception_mail(body,options = {})
+	def exception_mail(body,options = {})
   		from = options[:from]
   		to = options[:to]
   		subject = "NOTICE,your site raise an exception!"
   		body = body
   	  mail(:from=>from, :to => to, :subject => subject, :body => body)
-		end
+	end
 
 	end
 end
