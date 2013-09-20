bd6d9d96159b75fb0954c5372c181f293412223d
wxianfeng <wang.fl1429@gmail.com>
2013-09-21 01:14:29 +0800

    tab -> 2 space

--- lib/exception_log/mailer.rb
+++ lib/exception_log/mailer.rb
@@ -3,11 +3,11 @@ module ExceptionLog
 	class Mailer < ::ActionMailer::Base
 
 		def exception_mail(body,options = {})
-			from = options[:from]
-			to = options[:to]
-			subject = "NOTICE,your site raise an exception!"
-			body = body
-			mail(:from=>from, :to => to, :subject => subject, :body => body)
+  		from = options[:from]
+  		to = options[:to]
+  		subject = "NOTICE,your site raise an exception!"
+  		body = body
+  	  mail(:from=>from, :to => to, :subject => subject, :body => body)
 		end
 
 	end
